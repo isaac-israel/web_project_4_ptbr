7b708a963fa015a923ce9bfca6d64ba9243c0410
Isaac <82594024+isaac-israel@users.noreply.github.com>
2022-12-30 17:56:39 -0300

    Add button works, posts on js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,8 @@
 // Modal
 const edit = document.querySelector(".profile__button");
-const closeButton = document.querySelector(".popup__close");
-const modal = document.querySelector(".popup");
+const closeButton = document.querySelector(".edit__close");
+const modal = document.querySelector(".edit__modal");
 const fade = document.querySelector(".fade");
-
-
 
 function modalShow() {
   modal.classList.remove('popup_closed');
@@ -19,7 +17,7 @@ function modalClose() {
 edit.addEventListener('click', modalShow);
 closeButton.addEventListener('click', modalClose);
 
-// Form
+// Form do Edit
 const form = document.querySelector(".form");
 function formSubmit(e) {
   e.preventDefault();
@@ -51,10 +49,6 @@ name.placeholder = title.textContent;
 status.placeholder = subtitle.textContent;
 
 // Coloca post
-
-const posts = document.querySelector('.posts');
-const postTemplate = document.querySelector('.post__template').content;
-const postElement = postTemplate.querySelector('.post').cloneNode(true);
 const initialCards = [
   {
     name: "Vale de Yosemite",
@@ -83,10 +77,64 @@ const initialCards = [
 ];
 
 
-
 initialCards.forEach((item)=>{
+  const posts = document.querySelector('.posts');
+  const postTemplate = document.querySelector('.post__template').content;
+  const postElement = postTemplate.querySelector('.post').cloneNode(true);
+
   postElement.querySelector('.post__title').textContent=item.name;
   postElement.querySelector('.post__image').style.backgroundImage=`url(${item.link})`;
   posts.appendChild(postElement);
 });
 
+// Add Post
+const buttonAdd = document.querySelector('.button__add');
+const modalAdd = document.querySelector('.add__modal');
+const addClose = document.querySelector('.add__close');
+
+function addPost(){
+  modalAdd.classList.remove('popup_closed');
+  fade.classList.remove('fade_closed');
+}
+
+function closePost(){
+  modalAdd.classList.add('popup_closed');
+  fade.classList.add('fade_closed');
+}
+
+buttonAdd.addEventListener('click', addPost);
+addClose.addEventListener('click', closePost);
+
+// Form do Add
+const formAdd = document.querySelector('.form__add');
+
+function formAddSubmit(e){
+  e.preventDefault();
+
+  const addTitle = document.querySelector('.form__title');
+  const addLink = document.querySelector('.form__link');
+  const addTitleValue = addTitle.value;
+  const addLinkValue = addLink.value;
+  console.log(addTitle)
+
+  const arrayAux=[
+    {
+      name: `${addTitleValue}`,
+      link: `${addLinkValue}`
+    }
+  ];
+
+  initialCards.push(arrayAux);
+
+  const posts = document.querySelector('.posts');
+  const postTemplate = document.querySelector('.post__template').content;
+  const postElement = postTemplate.querySelector('.post').cloneNode(true);
+
+  postElement.querySelector('.post__title').textContent=addTitleValue;
+  postElement.querySelector('.post__image').style.backgroundImage=`url(${addLinkValue})`;
+  posts.appendChild(postElement);
+
+  closePost()
+}
+
+formAdd.addEventListener('submit', formAddSubmit)
